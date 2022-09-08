1ac1e7dd18fe5aeae4138a23002c1ab15b72312e
HarshKadam11101 <harshlk11101@gmail.com>
2022-09-08 23:58:55 +0530

    commit filestructure

---
+++ VehicalRenting/src/main/java/com/Sprint1/VehicleRenting/controller/LocationController.java
@@ -0,0 +1,10 @@
+package com.Sprint1.VehicleRenting.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+
+}
